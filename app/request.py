5e40137fcbe0986a243f3b22eeef32c2233d0154
martyminion <martkimwaweru@gmail.com>
2020-04-21 20:05:47 +0300

    adding category search to consume the api

--- app/request.py
+++ app/request.py
@@ -1,4 +1,53 @@
 from app import app
+import urllib.request, json
+from .models import movie
 
-#get the api key
+Movie = movie.Movie
+#getting the api key
 api_key = app.config['MOVIE_API_KEY']
+
+#Getting the movie Base URL
+base_url = app.config['MOVIE_API_BASE_URL']
+
+def process_results(movie_list):
+  '''
+  transforms the movie result into a list of objects
+
+  args:
+  movie_list : a list of dictionaries that contains movie details
+
+  returns:
+  movie_results : A list of movie objects
+  '''
+  movie_results = []
+  for movie_item in movie_list:
+    id = movie_item.get('')
+    title = movie_item.get('original_title')
+    overview = movie_item.get('overview')
+    poster = movie_item.get('poster_path')
+    vote_average = movie_item.get('vote_average')
+    vote_count = movie_item.get('vote_count')
+
+    if poster:
+      movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+      movie_results.append(movie_object)
+
+  return movie_results
+  
+def get_movies(category):
+  '''
+  function that gets the json response from the request ur;
+  '''
+  get_movies_url = base_url.format(category,api_key)
+
+  with urllib.request.urlopen(get_movies_url) as url:
+    get_movies_data = url.read()
+    get_movies_response = json.loads(get_movies_data)
+
+    movie_results = None
+
+    if get_movies_response['results']:
+      movie_results_list = get_movies_response['results']
+      movie_results = process_results(movie_results_list)
+
+  return movie_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #views
 
@@ -9,8 +10,15 @@ def index():
   view root page function that returns the index page and its data
   '''
   message = "Hello World"
+
+  #getting popular movie
+  popular_movies = get_movies('popular')
+  print(popular_movies)
+
   title = 'Home - Welcome to the Best Movie Review Website online '
-  return render_template('index.html',message = message, title = title)
+  return render_template('index.html',message = message, title = title, popular = popular_movies)
+
+  
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
