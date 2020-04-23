c287f4e6edaf3d2f278b5c046ea22728c1e8ec00
martyminion <martkimwaweru@gmail.com>
2020-04-23 15:59:44 +0300

    adds a search feature, with its form,view,and html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
 
 #views
 
@@ -18,7 +18,13 @@ def index():
 
 
   title = 'Home - Welcome to the Best Movie Review Website online '
-  return render_template('index.html',message = message, title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movie)
+
+  search_movie = request.args.get('movie_query')
+
+  if search_movie:
+    return redirect(url_for('search',movie_name = search_movie))
+  else:
+    return render_template('index.html',message = message, title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movie)
 
   
 
@@ -30,3 +36,14 @@ def movie(movie_id):
   movie = get_movie(movie_id)
   title = f"{movie.title}"
   return render_template('movie.html',movie = movie, title = title )
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+  '''
+  View Function to display the search results
+  '''
+  movie_name_list = movie_name.split(" ")
+  movie_name_format = "+".join(movie_name_list)
+  searched_movies = search_movie(movie_name_format)
+  title = f'search results for {movie_name}'
+  return render_template('search.html',movies = searched_movies,title = title)
