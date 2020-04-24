bd5cc20ffa3fe4fb8771676d4a29c2708aca7c7d
martyminion <martkimwaweru@gmail.com>
2020-04-24 13:29:34 +0300

    changing the aplication structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,49 @@
 from flask import Flask
-from .config import DevConfig
+from ..config import config_options
 from flask_bootstrap import Bootstrap
 
-#initializing the application
-app = Flask(__name__, instance_relative_config = True)
+bootstrap = Bootstrap()
 
-#Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+  app = Flask(__name__)
 
-#initializing bootstrap Extensions
-bootstrap = Bootstrap(app)
+  #creating app configurations
+  app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+  #initializing flask extensions
+  bootstrap.init_app(app)
+
+  #Registering the BluePrint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+  
+  #will add the views and the forms
+
+  return app
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+# from .config import DevConfig
+# from flask_bootstrap import Bootstrap
+
+# #initializing the application
+# app = Flask(__name__, instance_relative_config = True)
+
+# #Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# #initializing bootstrap Extensions
+# bootstrap = Bootstrap(app)
+
+# from app import views
+# from app import error
---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+class Config:
+
+  MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+  MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+  pass
+
+class DevConfig:
+  DEBUG = True
+
+config_options = {
+  'development': DevConfig,
+  'production' : ProdConfig
+}
+
+
+
+
+#before moving from the app directory
+# class Config:
+#   '''
+#   General configuration parent class
+#   '''
+#   MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+# class ProdConfig(Config):
+#   '''
+#   Production configuration class, a child of the Config class
+
+#   arguments:
+#   Config.. the parent configuration class with the general config settings
+#   '''
+#   pass
+
+# class DevConfig(Config):
+#   '''
+#   Development Configuration child class
+  
+#   Args:
+#   Config: the parent configuration class with the genral config settings
+#   '''
+
+#   DEBUG = True
